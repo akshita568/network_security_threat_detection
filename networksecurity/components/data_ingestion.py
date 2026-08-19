786907f7d1ede22e3de02b3965a7bdc3f76b80ec
Akshita Sharma <sakshita229@gmail.com>
2026-08-19 22:48:00 +0530

    data validation completed

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -105,7 +105,7 @@ class DataIngestion:
             self.split_data_as_train_test(dataframe)
         
             dataingestionartifact=DataIngestionArtifact(
-                trained_file_path=self.data_ingestion_config.training_file_path,
+                training_file_path=self.data_ingestion_config.training_file_path,
                 test_file_path=self.data_ingestion_config.testing_file_path
             )
             return dataingestionartifact
